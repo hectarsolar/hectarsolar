9ff307e800fcedaf5472eafa33b8738dd849b498
hectarsolar <hectarsolar@gmail.com>
2026-02-17 19:59:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// DARK MODE
-document.getElementById("theme-toggle").onclick = () => {
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-};
-
-// MOBILE MENU
-const sidebar = document.getElementById("sidebar");
-document.getElementById("menu-btn").onclick = () => {
-  sidebar.classList.toggle("open");
-};
-
-// COLLAPSIBLE TOP SECTION
-document.querySelector(".collapse-toggle").onclick = function () {
-  this.nextElementSibling.classList.toggle("show");
-};
+  toggle.textContent =
+    document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
